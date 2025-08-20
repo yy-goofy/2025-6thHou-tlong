3e136ad25f33285fb897249011a55e8b53632f7a
Tristan Long <tl129267@studrnt.wpsok.org>
2025-08-20 14:06:41 -0500

    added HW2

---
+++ HW2.py
@@ -0,0 +1,26 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW2
+
+#1. Print "Hello World!"
+print("Hello World")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+one = 1
+two = "hi there"
+yes = True
+#3. Print all three variables on the same print function (at the same time).
+print(one, two, yes)
+#4. Create a variable that asks the user to input an integer.
+six = (int(input("hi")))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+print(one + six)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print((one + six)/2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+yes= False
+#8. Print the value of the boolean variable.
+print(yes)
+#9. Create a variable with a number that contains decimals.
+god=1.3
+#10. Round the number from #9 up or down using the round function.
+print(round(god))
