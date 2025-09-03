c8d9ad95aff673e0de4b4dbe9377399203d5e329
Tristan Long <tl129267@studrnt.wpsok.org>
2025-09-03 14:03:28 -0500

    HW4

---
+++ HW4.py
@@ -0,0 +1,28 @@
+#Name:Tristan long
+#Class: 6th Hour
+#Assignment: HW4
+
+
+#1. Print Hello World!
+print("hello world")
+#1. Create a list with 5 strings containing 5 different names in it.
+jjk_list=["gojo","Yuji","Sukuna","Yuta","Maki"]
+#2. Append a new name onto the Name List.
+jjk_list.append("Megumi")
+#3. Print out the 4th name on the list.
+print(jjk_list[3])
+#4. Create a list with 4 different integers in it.
+int_list=[1,2,3,4]
+#5. Insert a new integer into the 2nd spot and print the new list.
+int_list.insert(5,5)
+print(int_list)
+#6. Sort the list from lowest to highest and print the sorted list.
+int_list.sort()
+print(int_list)
+#7. Add the 1st three numbers on the sorted list together and print the sum.
+print(sum(int_list))
+#8. Create a list with two strings, two variables, and too boolean values.
+mix_list=["1","2",1,2,True,True]
+print(mix_list)
+#9. Create a print statement that asks the user to input their own index value for the list on.
+index_input = input(mix_list)
