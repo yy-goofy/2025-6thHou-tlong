78214b23227157b22feab82b9a2f9d5bec8fa02c
yy-goofy <tl129267@student.wpsok.org>
2026-01-16 13:19:39 -0600

    Update HW17.py

--- HW17.py
+++ HW17.py
@@ -1,10 +1,31 @@
-#Name:
+#Name:Tristan long
 #Class: 6th Hour
 #Assignment: HW17
+
+import random
 
 #1. Create a def function that plays a single round of rock, paper, scissors where the user inputs
 #1 for rock, 2 for paper, or 3 for scissors and compares it to a random number generated to serve
 #as the "opponent's hand".
-
+def rps():
+    user = int(input("Enter 1 for rock, 2 for paper, 3 for scissors: "))
+    comp = random.randint(1, 3)
+    print("you:", user)
+    print("computer:", comp)
+    if user == comp:
+        print("tie")
+    elif user == 1 and comp == 3:
+        print("you win")
+    elif user == 2 and comp == 1:
+        print("you win")
+    elif user == 3 and comp == 2:
+        print("you win")
+    else:
+        print("you lose")
 #2. Create a def function that prompts the user to input if they want to play another round, and
 #repeats the RPS def function if they do or exits the code if they don't.
+def play():
+    again = "yes"
+    while again == "yes":
+        rps()
+        again = input("play again? ").lower()
