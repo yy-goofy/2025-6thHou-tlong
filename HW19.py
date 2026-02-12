635323fdabf46b31a74285d46c4075986bc9d43f
Tristan Long <tl129267@studrnt.wpsok.org>
2026-02-12 13:50:31 -0600

    HW19

---
+++ HW19.py
@@ -0,0 +1,21 @@
+#Name:Tristan long
+#Class: 6th Hour
+#Assignment: HW19
+
+#1. Import the def functions created in problem 1-4 from HW16
+from HW16 import hello_world, addition, animals ,loop
+#2. Call the functions here and run HW19
+hello_world()
+addition(1, 2, 3)
+animals("dog", "cat", "mouse, frog, lion, snake")
+loop(number)
+#3. Delete all calls for problem 5 in HW16 and run HW19 again.
+
+#4. Create a try catch that tries to print variable x (which has no value), but prints "Hello World!" instead.
+
+#5. Create a try catch that tries to divide 100 by whatever number the user inputs, but prints an exception for Divide By Zero errors.
+
+#6. Create a variable that asks the user for a number. If the user input is not an integer, prints an exception for Value errors.
+
+#7. Create a while loop that counts down from 5 to 0, but raises an exception when it counts below zero.
+
