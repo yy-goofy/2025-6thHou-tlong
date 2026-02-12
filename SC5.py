8519532417d219aca0f5a8ffd6a415c69e7267d1
Tristan Long <tl129267@studrnt.wpsok.org>
2026-02-12 13:50:11 -0600

    SC5

---
+++ SC5.py
@@ -0,0 +1,17 @@
+#Name:
+#Class: 5th Hour
+#Assignment: SC5
+
+#Import all of SC4 here
+from HW16 import func
+
+listAverage = 0
+
+def final_average():
+    global listAverage
+    listAverage = 5    #Calculate the sum of the list by the length of the list here
+    return listAverage
+
+final_average()
+
+print(listAverage)
