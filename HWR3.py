62454ada07ce1fed4846b9d1c88b108aa0f8c26d
Tristan Long <tl129267@studrnt.wpsok.org>
2026-03-25 14:07:25 -0500

    HWR3

---
+++ HWR3.py
@@ -0,0 +1,19 @@
+#Name:Tristan long
+#Class: 6th Hour
+#Assignment: HW-R3
+
+
+#1. import random and print "Hello World!"
+import random
+print("hello world")
+#2. Create three variables that each randomly generate an integer between 1 and 10, print each number on the same line.
+a = random.randint(1,10)
+b = random.randint(1,10)
+c = random.randint(1,10)
+print(a,b,c)
+#3. Create a list containing 5 strings listing 5 colors.
+srtings_list = ["blue,green,pink,red,purple"]
+print(srtings_list)
+#4. Use a function to randomly choose one of the 5 colors from the list and print the result.
+
+#5. Create an if statement that determines which of the three variables from #2 is the lowest.
