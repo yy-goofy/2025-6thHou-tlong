d37eed4caeec8d3448cd91245e8556a283a0a593
Tristan Long <tl129267@studrnt.wpsok.org>
2026-03-25 14:07:50 -0500

    HWR1

---
+++ HWR1.py
@@ -0,0 +1,37 @@
+#Name:Tristan long
+#Class: 6th Hour
+#Assignment: HW-R1
+from operator import truediv
+
+#1. Print "Hello World!"
+print("Hello World!")
+#2. Create three variables with strings as values.
+a = "mom"
+b = "dad"
+c = "sis"
+#3. Create three variables with booleans as values.
+t = True
+f = False
+j = True
+#4. Create three variables with integers as values.
+g = 66
+h = 55
+k = 44
+#5. Create a variable that asks for the user's name and prints it.
+name = input("What is your name?")
+print("welcome back " + name + "!")
+#6. Add together the three variables from #4 and print the result.
+if g + h + k:
+    print(g + h + k)
+#7. Subtract the second variable from the first from #4 and print the result.
+if h - g:
+    print(h - g)
+#8. Multiply the third variable from #4 to the result of #6 and print the result.
+if k * 165:
+    print(k * 165)
+#9. Create a variable that asks the user for a number with a decimal (a float).
+num = float(input("Enter a decimal:"))
+print(num)
+#10. Round the variable from #9 to the nearest whole number and print the result.
+print(round(num))
+#11. Find the remainder (hint: modulo) of 355/113. Print the result.
