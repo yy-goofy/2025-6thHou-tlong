89e4cab8c21332c86d88941a7c18a7b3d624b87d
Tristan Long <tl129267@studrnt.wpsok.org>
2026-03-25 14:09:24 -0500

    HWR1

---
+++ HWR2.py
@@ -0,0 +1,30 @@
+#Name:Tristan
+#Class: 6th Hour
+#Assignment: HW-R2
+
+
+#1. Print "Hello World!"
+print("hello world!")
+#2. Create an empty list.
+random_list = []
+print(random_list)
+#3. Create a list that contains the names of everyone in the classroom.
+name_list = ["Connor", "Devon", "Alaya", "Shane", "Ally", "Tristan", "Ethan", "Greg",]
+#4. Print the list from #3, sort the list, then print the list again.
+print(name_list)
+name_list.sort()
+print(name_list)
+#5. Append 5 different integers into the empty list from #2 and print the list.
+random_list.append("1, 2, 3, 4, 5")
+print(random_list)
+#6. Add together the middle three numbers in the list from #2 and print the result.
+if 2+3+4:
+    print(2+3+4)
+#7. Remove the very first number in the list from #2. Print the new first number.
+random_list.pop("1")
+print(random_list)
+#8. Create a dictionary with three keys with respective values: your name, your grade, and your favorite color.
+
+#9. Using the update function, add a fourth key and value determining your favorite candy.
+
+#10. Print ONLY the values of the dictionary from #8.
