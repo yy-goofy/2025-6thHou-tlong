28ec9967b7c1f13ec4fbf984b53258a8a57f8e5b
Tristan Long <tl129267@studrnt.wpsok.org>
2026-03-27 13:44:48 -0500

    HWR3

--- HWR3.py
+++ HWR3.py
@@ -12,8 +12,16 @@ b = random.randint(1,10)
 c = random.randint(1,10)
 print(a,b,c)
 #3. Create a list containing 5 strings listing 5 colors.
-srtings_list = ["blue,green,pink,red,purple"]
-print(srtings_list)
+strings_list = ["blue,green,pink,red,purple"]
+print(strings_list)
 #4. Use a function to randomly choose one of the 5 colors from the list and print the result.
-
+print(random.choice(strings_list))
 #5. Create an if statement that determines which of the three variables from #2 is the lowest.
+if a <= b and a <= c:
+    print(f"a is the lowest number with {a}")
+if b <= a and b <= c:
+    print(f"b is the lowest number {b}")
+if c <= b and c <= a:
+    print(f"c is the lowest number{c}")
+if a == b == c:
+    print("all are equal")
