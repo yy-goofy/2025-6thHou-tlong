25a023ed0611d8355756599d2820c3b78d351a68
Tristan Long <tl129267@studrnt.wpsok.org>
2026-04-13 13:57:03 -0500

    HWR6

---
+++ HWR6.py
@@ -0,0 +1,23 @@
+#Name:Tristan long
+#Class: 6th Hour
+#Assignment: HW-R6
+
+#1. Create a def function that prints out "Hello World!". Call the function.
+def hello_world():
+    print("Hello World!")
+hello_world()
+#2. Create a def function that prints your name. Call the function with the name as the argument.
+def tristan():
+    print("tristan")
+tristan()
+#3. Create a def function that calculates the average of a list. Call the function with the list as the argument.
+c_list = (1,2,3)
+def average():
+    print(c_list)
+
+#4. Call the function from #3 but with a new list of different numbers.
+average()
+#5. Create a def function that takes two numbers as arguments, x and y. Inside the function, create a for loop
+#with a range of 10. Inside the loop, print x, make z equal the sum of x and y, make x equal y, then y equal z.
+
+#6. Call the function from #5 with the arguments for x and y being 0 and 1.
